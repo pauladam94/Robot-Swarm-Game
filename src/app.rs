bda264d08848705acc37a482ecce05ca071a1bac
Paul ADAM <poladam2002@gmail.com>
2023-12-19 09:36:09 +0100

    stable version to present an article about this problem

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
+use crate::compute::{Grid, GRID_SIZE};
 use egui::Vec2;
 
-use crate::compute::Grid;
-
+#[derive(Clone, Copy, PartialEq)]
 pub enum Move {
     Left,
     Right,
@@ -23,14 +23,20 @@ pub struct TemplateApp {
     grid: Grid,
     last_move: Option<Move>,
     spacing: f32,
+    relocation: bool,
+    step: usize,
+    next_move: Option<Move>,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            grid: Grid::new(),
+            grid: Grid::simple_three_particule(),
             last_move: None,
-            spacing: 5.0,
+            spacing: 7.0,
+            relocation: false,
+            step: 1,
+            next_move: None,
         }
     }
 }
@@ -43,70 +49,79 @@ impl TemplateApp {
 
 impl eframe::App for TemplateApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self {
-            grid,
-            last_move,
-            spacing,
-        } = self;
-
         egui::SidePanel::right("Window").show(ctx, |ui| {
             egui::widgets::global_dark_light_mode_buttons(ui);
 
-            ui.add(egui::Slider::new(spacing, 0.0..=20.0).text("Spacing"))
+            ui.add(egui::Slider::new(&mut self.spacing, 0.0..=20.0).text("Spacing"))
                 .on_hover_text("Spacing between cells");
 
-            if let Some(last_move) = last_move {
-                ui.heading(format!("Last Move : {}", last_move));
-            }
-
-            if ui
-                .add(egui::Button::new("Up").min_size(Vec2::new(100.0, 100.0)))
-                .clicked()
-                || ctx.input(|i| i.keys_down.contains(&egui::Key::ArrowUp))
-            {
-                grid.up_step();
-                *last_move = Some(Move::Up);
-            }
-
             ui.horizontal(|ui| {
-                if ui
-                    .add(egui::Button::new("Left").min_size(Vec2::new(100.0, 100.0)))
-                    .clicked()
-                    || ctx.input(|i| i.keys_down.contains(&egui::Key::ArrowLeft))
+                if ui.add(egui::Button::new("Up")).clicked()
+                    || ctx.input(|i| i.key_pressed(egui::Key::ArrowUp))
                 {
-                    grid.left_step();
-                    *last_move = Some(Move::Left);
+                    self.next_move = Some(Move::Up);
                 }
 
-                if ui
-                    .add(egui::Button::new("Right").min_size(Vec2::new(100.0, 100.0)))
-                    .clicked()
-                    || ctx.input(|i| i.keys_down.contains(&egui::Key::ArrowRight))
+                if ui.add(egui::Button::new("Left")).clicked()
+                    || ctx.input(|i| i.key_pressed(egui::Key::ArrowLeft))
                 {
-                    grid.right_step();
-                    *last_move = Some(Move::Right);
+                    self.next_move = Some(Move::Left);
+                }
+
+                if ui.add(egui::Button::new("Right")).clicked()
+                    || ctx.input(|i| i.key_pressed(egui::Key::ArrowRight))
+                {
+                    self.next_move = Some(Move::Right);
+                }
+
+                if ui.add(egui::Button::new("Down")).clicked()
+                    || ctx.input(|i| i.key_pressed(egui::Key::ArrowDown))
+                {
+                    self.next_move = Some(Move::Down);
                 }
             });
 
-            if ui
-                .add(egui::Button::new("Down").min_size(Vec2::new(100.0, 100.0)))
-                .clicked()
-                || ctx.input(|i| i.keys_down.contains(&egui::Key::ArrowDown))
-            {
-                grid.down_step();
-                *last_move = Some(Move::Down);
+            if ui.add(egui::Button::new("Map 3 Particule")).clicked() {
+                self.grid = Grid::simple_three_particule();
             }
 
-            if ui
-                .add(egui::Button::new("Reverse").min_size(Vec2::new(100.0, 100.0)))
-                .clicked()
-            {
-                grid.reverse();
+            if ui.add(egui::Button::new("Map 1 Particule")).clicked() {
+                self.grid = Grid::simple_one_particule();
+            }
+
+            if ui.add(egui::Button::new("Map 4 Particule")).clicked() {
+                self.grid = Grid::complex_four_particule();
+            }
+
+            if ui.add(egui::Button::new("Reverse")).clicked() {
+                self.grid.reverse();
+            }
+
+            ui.checkbox(&mut self.relocation, "Relocation Problem");
+            // slider for the number of steps
+            ui.add(egui::Slider::new(&mut self.step, 1..=GRID_SIZE).text("Step"))
+                .on_hover_text("Number of steps to do");
+
+            ui.separator();
+            ui.heading(format!("Last Move :"));
+            if let Some(last_move) = &self.last_move {
+                ui.label(
+                    egui::RichText::new(format!("{last_move}"))
+                        .font(egui::FontId::proportional(70.0)),
+                );
+            }
+
+            if let Some(next_move) = self.next_move {
+                for _ in 0..self.step {
+                    self.grid.step(next_move, &mut self.last_move);
+                }
+                self.next_move = None;
             }
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            self.grid.draw(ui, *spacing);
+            self.grid
+                .draw(ui, self.spacing, self.relocation, Vec2::new(20f32, 20f32));
         });
     }
 }
--- src/compute.rs
+++ src/compute.rs
@@ -1,6 +1,8 @@
 use egui::{Color32, Pos2, Rect, Stroke, Vec2};
 
-const GRID_SIZE: usize = 9;
+pub const GRID_SIZE: usize = 10;
+const J0: usize = 2;
+const I0: usize = 2;
 
 #[derive(Copy, Clone, PartialEq)]
 enum Cell {
@@ -26,12 +28,9 @@ impl Cell {
         }
     }
 
-    pub fn rounding(&self) -> f32 {
-        match self {
-            Cell::Empty => 0f32,
-            Cell::Wall => 0f32,
-            Cell::Particule => 10f32,
-        }
+    pub fn rounding(&self, size: f32) -> f32 {
+        let rounding = size / (8 * GRID_SIZE) as f32;
+        rounding
     }
 }
 
@@ -40,35 +39,113 @@ pub struct Grid {
 }
 
 use Cell::*;
+
+use crate::app::Move;
 impl Grid {
-    pub fn new() -> Grid {
+    pub fn simple_three_particule() -> Grid {
         Grid {
             cells: [
-                [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
-                [Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall],
+                [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
                 [
-                    Wall, Empty, Particule, Empty, Empty, Empty, Empty, Empty, Wall,
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
                 ],
-                [Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall],
-                [Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall],
                 [
-                    Wall, Empty, Empty, Empty, Particule, Empty, Empty, Empty, Wall,
+                    Wall, Empty, Particule, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
                 ],
-                [Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall],
                 [
-                    Wall, Empty, Empty, Particule, Wall, Empty, Empty, Empty, Wall,
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
                 ],
-                [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Particule, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Particule, Wall, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
+            ],
+        }
+    }
+
+    pub fn simple_one_particule() -> Grid {
+        Grid {
+            cells: [
+                [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
+                [
+                    Wall, Empty, Wall, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Wall, Wall, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Wall, Wall, Wall, Empty, Empty, Empty, Empty, Wall, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Wall, Empty, Particule, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Wall, Empty, Empty, Empty, Empty, Wall, Wall, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Wall, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Wall, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
+            ],
+        }
+    }
+
+    pub fn complex_four_particule() -> Grid {
+        Grid {
+            cells: [
+                [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Particule, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [
+                    Wall, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+                ],
+                [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
             ],
         }
     }
 
     pub fn left_step(&mut self) {
-        let previous_cells = self.cells.clone();
         for i in 0..GRID_SIZE {
-            for j in (0..GRID_SIZE).rev() {
-                if previous_cells[i][j] == Cell::Particule {
-                    if j != 0 && previous_cells[i][j - 1] == Cell::Empty {
+            for j in 0..GRID_SIZE {
+                if self.cells[i][j] == Cell::Particule {
+                    if j != 0 && self.cells[i][j - 1] == Cell::Empty {
                         self.cells[i][j] = Cell::Empty;
                         self.cells[i][j - 1] = Cell::Particule;
                     }
@@ -78,11 +155,10 @@ impl Grid {
     }
 
     pub fn right_step(&mut self) {
-        let previous_cells = self.cells.clone();
         for i in 0..GRID_SIZE {
-            for j in 0..GRID_SIZE {
-                if previous_cells[i][j] == Cell::Particule {
-                    if j != GRID_SIZE - 1 && previous_cells[i][j + 1] == Cell::Empty {
+            for j in (0..GRID_SIZE).rev() {
+                if self.cells[i][j] == Cell::Particule {
+                    if j != GRID_SIZE - 1 && self.cells[i][j + 1] == Cell::Empty {
                         self.cells[i][j] = Cell::Empty;
                         self.cells[i][j + 1] = Cell::Particule;
                     }
@@ -92,11 +168,10 @@ impl Grid {
     }
 
     pub fn up_step(&mut self) {
-        let previous_cells = self.cells.clone();
-        for i in 0..GRID_SIZE {
-            for j in 0..GRID_SIZE {
-                if previous_cells[i][j] == Cell::Particule {
-                    if i != 0 && previous_cells[i - 1][j] == Cell::Empty {
+        for j in 0..GRID_SIZE {
+            for i in 0..GRID_SIZE {
+                if self.cells[i][j] == Cell::Particule {
+                    if i != 0 && self.cells[i - 1][j] == Cell::Empty {
                         self.cells[i][j] = Cell::Empty;
                         self.cells[i - 1][j] = Cell::Particule;
                     }
@@ -106,17 +181,26 @@ impl Grid {
     }
 
     pub fn down_step(&mut self) {
-        let previous_cells = self.cells.clone();
-        for i in (0..GRID_SIZE).rev() {
-            for j in 0..GRID_SIZE {
-                if previous_cells[i][j] == Cell::Particule {
-                    if i != GRID_SIZE - 1 && previous_cells[i + 1][j] == Cell::Empty {
+        for j in 0..GRID_SIZE {
+            for i in (0..GRID_SIZE).rev() {
+                if self.cells[i][j] == Cell::Particule {
+                    if i != GRID_SIZE - 1 && self.cells[i + 1][j] == Cell::Empty {
                         self.cells[i][j] = Cell::Empty;
                         self.cells[i + 1][j] = Cell::Particule;
                     }
                 }
             }
         }
+    }
+
+    pub fn step(&mut self, m: Move, last_move: &mut Option<Move>) {
+        match m {
+            Move::Up => self.up_step(),
+            Move::Down => self.down_step(),
+            Move::Left => self.left_step(),
+            Move::Right => self.right_step(),
+        }
+        *last_move = Some(m);
     }
 
     pub fn reverse(&mut self) {
@@ -131,21 +215,25 @@ impl Grid {
         }
     }
 
-    pub fn draw(&self, ui: &mut egui::Ui, spacing: f32) {
-        let available_height = ui.available_height() + spacing * GRID_SIZE as f32;
-        let available_width = ui.available_width() + spacing * GRID_SIZE as f32;
+    pub fn draw(&self, ui: &mut egui::Ui, spacing: f32, relocation: bool, offset: Vec2) {
+        let available_width = ui.available_width() - spacing * GRID_SIZE as f32 - offset.x;
+        let available_height = ui.available_height() - spacing * GRID_SIZE as f32 - offset.y;
         let min = available_height.min(available_width);
         let cell_height = min / GRID_SIZE as f32;
         for i in 0..GRID_SIZE {
             for j in 0..GRID_SIZE {
                 let cell = self.cells[i][j];
-                let fill_color = cell.fill_color();
-                let stroke = cell.stroke(0.1f32);
-                let rounding = cell.rounding();
+                let fill_color = if relocation && i == I0 && j == J0 && cell == Cell::Empty {
+                    Color32::from_rgba_premultiplied(255, 0, 0, 80)
+                } else {
+                    cell.fill_color()
+                };
+                let stroke = cell.stroke(0f32);
+                let rounding = cell.rounding(min);
                 let pos = Pos2::new(
                     j as f32 * cell_height + j as f32 * spacing,
                     i as f32 * cell_height + i as f32 * spacing,
-                );
+                ) + offset;
                 ui.painter().rect_filled(
                     Rect::from_min_size(pos, Vec2::new(cell_height, cell_height)),
                     rounding,
